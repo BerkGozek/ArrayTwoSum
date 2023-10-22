61a4a3d0e698a31415655ab62ef6783b346b195c
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-10-22 20:44:17 +0300

    Komm Susser Todd

---
+++ TuSum/TuSum/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TuSum
+//
+//  Created by Berk Gozek on 22/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TuSum/TuSum/TuSumApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuSumApp.swift
+//  TuSum
+//
+//  Created by Berk Gozek on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TuSumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
