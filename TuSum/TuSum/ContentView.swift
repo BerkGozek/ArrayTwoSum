9300f1e81d55dd95ac9a0172451c16531f511708
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-10-23 20:22:15 +0300

    somethings. I dont remember

--- TuSum/TuSum/ContentView.swift
+++ TuSum/TuSum/ContentView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State public var numberInput : Int = 0
+    @State public var numberArray: [Int] = []
     var body: some View {
         VStack {
+            Button("Enter Number to Array"){
+                
+            }
+            TextField("Enter Number", value: $numberInput, formatter: NumberFormatter())
+                .multilineTextAlignment(.center)
+            List {
+                ForEach(numberArray.sorted(), id: \.self) { number in
+                    Text("\(number)")
+                }
+            }
+
+
             
         }
         .padding()
